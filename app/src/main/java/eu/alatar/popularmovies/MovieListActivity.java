fb0c85095bf190ed75b4b9db97d9cbf3a60dbe1d
alatar- <contact@alatar.eu>
2017-02-10 10:44:55 +0100

    Implement basic RecyclerView for Movie List.

--- app/src/main/java/eu/alatar/popularmovies/MovieListActivity.java
+++ app/src/main/java/eu/alatar/popularmovies/MovieListActivity.java
@@ -2,12 +2,32 @@ package eu.alatar.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.ListViewCompat;
+import android.support.v7.widget.RecyclerView;
 
 public class MovieListActivity extends AppCompatActivity {
+
+    private RecyclerView mMoviesRecyclerView;
+    private MoviesAdapter mMoviesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.movies_list);
+
+        mMoviesRecyclerView = (RecyclerView) findViewById(R.id.rv_movies_list);
+
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        mMoviesRecyclerView.setLayoutManager(layoutManager);
+
+        mMoviesAdapter = new MoviesAdapter();
+
+        mMoviesRecyclerView.setAdapter(mMoviesAdapter);
     }
+
+
+
 }
---
+++ app/src/main/java/eu/alatar/popularmovies/MoviesAdapter.java
@@ -0,0 +1,54 @@
+package eu.alatar.popularmovies;
+
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by kam on 10/02/17.
+ */
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesAdapterViewHolder> {
+
+    private String[] mData;
+
+    public MoviesAdapter() {
+        this.mData = new String[]{"El1", "Element2", "eli3"};
+    }
+
+    @Override
+    public MoviesAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        boolean shouldAttachToParentImmediately = false;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_poster, parent, shouldAttachToParentImmediately);
+
+        return new MoviesAdapterViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(MoviesAdapterViewHolder holder, int position) {
+        String text = mData[position];
+        holder.mMoviesTextView.setText(text);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mData == null) {
+            return 0;
+        } else {
+            return mData.length;
+        }
+    }
+
+    public class MoviesAdapterViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mMoviesTextView;
+
+        public MoviesAdapterViewHolder(View itemView) {
+            super(itemView);
+            mMoviesTextView = (TextView) itemView.findViewById(R.id.tv_movie_poster);
+
+        }
+    }
+}
